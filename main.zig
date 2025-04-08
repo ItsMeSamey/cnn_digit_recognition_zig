2356317d57cb1c935b52e1e491b4d5c7e53da5a9
ItsMeSamey <sameychain5041@gmail.com>
2025-04-08 12:08:46 +0530

    general updates

--- main.zig
+++ main.zig
@@ -5,13 +5,13 @@ const Activation = @import("functions_activate.zig");
 const Loss = @import("functions_loss.zig");
 const MNIST = @import("read_minst.zig");
 
-const cnn = CNN(f64, 28, 28, Loss.MeanSquaredError, [_]Layer.LayerType{
+const cnn = CNN(f64, 28, 28, Loss.ClosestError, [_]Layer.LayerType{
   Layer.getFlattener(),
-  Layer.getDense(256, Activation.ReLU),
-  Layer.getDense(128, Activation.Tanh),
-  Layer.getDense(64, Activation.ReLU),
-  Layer.getDense(16, Activation.Sigmoid),
-  Layer.getDense(10, Activation.NormalizeSquared),
+  // Layer.getDense(14*14*2, Activation.Tanh),
+  Layer.getDense(14*14, Activation.ReLU),
+  Layer.getDense(7*7, Activation.Sigmoid),
+  Layer.getDense(4*7, Activation.Tanh),
+  Layer.getDense(10, Activation.ReLU),
 });
 
 const MNISTIterator = MNIST.GetMinstIterator(28, 28);
@@ -25,14 +25,14 @@ pub fn main() !void {
   var rng = std.Random.DefaultPrng.init(@bitCast(std.time.timestamp()));
 
   var trainer = cnn.Trainer{.layers = undefined};
-  const mnist_iterator = try MNISTIterator.init("./datasets/train-images.idx3-ubyte", "./datasets/train-labels.idx1-ubyte", allocator);
+  var mnist_iterator = try MNISTIterator.init("./datasets/train-images.idx3-ubyte", "./datasets/train-labels.idx1-ubyte", allocator);
   defer mnist_iterator.free(allocator);
 
   for (0..32) |i| {
-    trainer.train(mnist_iterator, .{
+    trainer.train(mnist_iterator.randomIterator(rng.random(), mnist_iterator.count*2), .{
       .verbose = true,
-      .batch_size = @intCast(i + 8),
-      .learning_rate = @as(f64, 1) / @as(f64, @floatFromInt(1 + i)),
+      .batch_size = @intCast(i + 32),
+      .learning_rate = @as(f64, 0.01) / @as(f64, @floatFromInt(1 + i)),
       .rng = rng.random(),
     });
   }
@@ -42,7 +42,7 @@ pub fn main() !void {
   const mnist_test_iterator = try MNISTIterator.init("./datasets/t10k-images.idx3-ubyte", "./datasets/t10k-labels.idx1-ubyte", allocator);
   defer mnist_test_iterator.free(allocator);
 
-  const loss = tester.@"test"(mnist_test_iterator, false);
-  std.debug.print("\n>>Final Loss: {d:.3}\n", .{loss*100});
+  const accuracy = tester.@"test"(mnist_test_iterator);
+  std.debug.print("\n>>Final Accuracy: {d:.3}%\n", .{accuracy*100});
 }
 
